dbc3034b3d85b761b59a7e82187fea21443fa214
Joel Stidley <30506169+joelst@users.noreply.github.com>
2018-05-30 16:31:23 -0700

    Add in test files

---
+++ tests/Reset-PCTests.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+Removes and readds the PartnerCenterModule for testing
+
+.PARAMETER webAppAuth 
+Authenticates using the Web App authentication information provided.
+
+.PARAMETER appUserAuth 
+Authenticates using App+User authentication information provided.
+
+.PARAMETER credentials 
+This is a credential object created using Get-Credential.
+
+.EXAMPLE
+.\Reset-PCTest.ps1 -AppUserAuth -credentials $creds -CspDomain ContosoCsp.OnMicrosoft.com -CspAppId $CspApppId
+
+Resets the test using AppUser Authentication.
+.NOTES
+You need to have a authentication credential already established before running this cmdlet.
+
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $false)][switch]$webAppAuth,
+    [Parameter(Mandatory = $false)][switch]$appUserAuth,
+    [Parameter(Mandatory = $true)][PSCredential]$credentials,
+    [Parameter(Mandatory = $true)][string]$CspDomain,
+    [Parameter(Mandatory = $false)][string]$CspClientSecret,
+    [Parameter(Mandatory = $false)][string]$CspAppId,
+    [Parameter(Mandatory = $false)][string]$CspWebAppId
+)
+
+# If the module is already imported, remove it.
+$x = Get-Module -Name PartnerCenterModule
+if ($x -ne $null) {
+    Remove-Module -Name PartnerCenterModule    
+}
+
+# Import the latest version of the module
+Import-Module ..\PartnerCenterModule\PartnerCenterModule.psd1 -Force
+
+if ($WebAppAuth) {
+    # App Auth
+    Add-PCAuthentication -cspAppID $cspWebappId -cspDomain $cspDomain -cspClientSecret $cspClientSecret -Verbose
+}
+elseif ($AppUserAuth) {
+
+    # App+User Auth
+    Add-PCAuthentication -cspAppID $cspAppId -cspDomain $cspDomain -credential $credentials -Verbose
+}
+
+
---
+++ tests/Test-PCCmdlets.ps1
@@ -0,0 +1,145 @@
+<#
+.SYNOPSIS
+Tests all of the cmdlets in the PartnerCenterModule.
+
+.DESCRIPTION
+This will test all of the cmdlets in the module.
+
+.PARAMETER saToken 
+The authentication token you have created with your Partner Center credentials.
+
+.EXAMPLE
+Return a list of assigned licenses for the partner.
+
+Test-PCCmdlets.ps1
+
+.NOTES
+You need to have a authentication credential already established before running this cmdlet.
+
+#>
+
+<#[CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $false)][Securestring]$credentials,
+        [Parameter(Mandatory = $false)][string]$cspDomain,
+        [Parameter(Mandatory = $false)][Securestring]$cspClientSecret,
+        [Parameter(Mandatory = $false)][string]$cspAppId,
+        [Parameter(Mandatory = $false)][string]$cspWebAppId
+    )
+#>
+
+## Get Verbs
+# Get-PCCustomerTests
+
+Get-PCCustomer -Verbose | Format-Table -AutoSize
+Get-PCCustomer -all -Verbose | Format-Table -AutoSize
+
+$tenant = Get-PCCustomer | Select-Object -Last 1
+Get-PCCustomer -tenantid $tenant.id -Verbose | Format-Table -AutoSize
+
+Get-PCCustomer -startswith p -Limit 2 -Verbose
+
+# Get-PCAddressRulesByMarket
+Get-PCAddressRulesByMarket -countryid US -Verbose
+
+# Get-PCAuditRecord
+Get-PCAuditRecord -startDate 2018-05-10 -Verbose
+
+#Get-PCAzureRateCard
+Get-PCAzureRateCard -currency USD -region US -Verbose
+
+Get-PCAzureResourceMonthlyUsageRecord
+Get-PCBillingProfile
+
+Get-PCCustomerBillingProfile
+Get-PCCustomerCompanyProfile
+Get-PCCustomerLicenseDeployment
+Get-PCCustomerLicenseUsage
+Get-PCCustomerServiceCostSummary
+Get-PCCustomerUsageSummary
+Get-PCCustomerUser
+Get-PCCustomerUserRole
+Get-PCCustomerRoleMember
+Get-PCCustomerRole
+Get-PCCustomerRelationship
+Get-PCDomainAvailability
+Get-PCInvoice
+Get-PCInvoiceLineItem
+Get-PCLegalBusinessProfile
+Get-PCLicenseDeployment
+Get-PCLicenseUsage
+Get-PCManagedService
+Get-PCMpnProfile
+Get-PCOffer
+Get-PCOfferCategoriesByMarket
+Get-PCOrder
+Get-PCOrganizationProfile
+Get-PCSaToken
+Get-PCSpendingBudget
+Get-PCSR
+Get-PCSRTopic
+Get-PCSubscribedSKU
+Get-PCSubscription
+Get-PCSupportProfile
+Get-PCUsage
+Get-PCUsage2
+Get-PCManagedServices
+Get-PCSRTopics
+Get-PCIndirectReseller
+Get-PCResellerCustomer
+
+# Deprecated and no longer tested Get- cmdlets
+<#
+Get-PCCustomerLicensesDeployment
+Get-PCCustomerLicensesUsage
+Get-PCInvoiceLineItems
+Get-PCResellerCustomers
+Get-PCAzureResourceMonthlyUsageRecords
+Get-PCCustomerRelationships
+Get-PCIndirectResellers
+Get-PCLicensesUsage
+Get-PCLicensesDeployment
+Get-PCAuditRecords
+Get-PCSubscribedSKUs
+#>
+
+# This should be used as part of other cmdlets.
+#Select-PCCustomer
+
+# Add tests for these only in the integration sandbox
+<#
+New-PCAddress
+
+New-PCCustomerBillingProfile
+New-PCCustomerCompanyProfile
+New-PCCustomerDefaultAddress
+
+New-PCOrder
+New-PCOrderLineItem
+New-PCRelationshipRequest
+New-PCSaToken
+New-PCSR
+
+New-PCCustomer
+Remove-PCCustomer
+
+New-PCCustomerUser
+Remove-PCCustomerUser
+
+Add-PCCustomerRoleMember
+Remove-PCCustomerRoleMember
+
+Restore-PCCustomerUser
+
+Set-PCBillingProfile
+Set-PCCustomerBillingProfile
+Set-PCCustomerUser
+Set-PCLegalBusinessProfile
+Set-PCOrganizationProfile
+Set-PCSpendingBudget
+Set-PCSR
+Set-PCSubscription
+Set-PCSupportProfile
+
+Test-PCAddress
+#>
